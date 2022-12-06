b8f64f7d64f574815889021e3ba2d0d9ba529776
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2022-12-06 23:50:29 +0100

    Task 1 completed

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,9 @@
-const numberOfCategories = ;
+const itemCategories = document.querySelectorAll(".item");
+console.log("Number of categories:", itemCategories.length);
+("");
+
+const itemList = document.querySelectorAll(".item");
+
+itemList.forEach((el) => {
+  console.log("Category:", el.querySelector("h2").textContent), console.log("Elements:", el.querySelector("ul").childElementCount);
+});

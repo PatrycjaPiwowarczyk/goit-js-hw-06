ddb7886f44c1ae50df370b5fab857733ae9bc0ee
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2022-12-07 00:05:59 +0100

    Task 2 completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientList = document.querySelector("#ingredients");
+
+ingredients.forEach((el, index) => {
+  const ingredient = document.createElement("li");
+  ingredient.textContent = ingredients[index];
+  ingredient.classList.add("item");
+  ingredientList.append(ingredient);
+});

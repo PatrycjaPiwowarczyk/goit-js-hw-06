31f1366b4d55e197c8b6a2a744cb89cdd023d3e9
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2022-12-11 16:43:36 +0100

    Task 10 completed

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createButton = document.querySelector("button[data-create]");
+const destroyButton = document.querySelector("button[data-destroy]");
+const input = document.querySelector("input");
+const boxesBox = document.querySelector("#boxes");
+
+const createBoxes = (amount) => {
+  amount = parseInt(input.value);
+  for (let i = 1; i <= amount; i++) {
+    const newBox = document.createElement("div");
+    boxesBox.append(newBox);
+    let sizer = 20 + i * 10;
+    newBox.style.width = `${sizer}px`;
+    newBox.style.height = `${sizer}px`;
+    newBox.style.backgroundColor = getRandomHexColor();
+  }
+};
+
+const destroyBoxes = (amount) => {
+  amount = parseInt(input.value);
+  for (let i = 1; i <= amount; i++) {
+    boxesBox.remove("div");
+  }
+  location.reload();
+};
+
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);

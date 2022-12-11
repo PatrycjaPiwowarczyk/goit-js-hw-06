600254780f39b265561e0261e01be3a1b03922e4
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2022-12-11 14:28:04 +0100

    Task 9 completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const span = document.querySelector("span");
+const button = document.querySelector("button");
+
+const colorChange = () => {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+};
+
+button.addEventListener("click", colorChange);

5910e519d4ac19f4bd82558731cf9b787431b3a8
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2022-12-11 13:09:01 +0100

    Task 7 completed

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const input = document.querySelector("#font-size-control");
+const span = document.querySelector("#text");
+
+const updateSpan = () => {
+  span.style.fontSize = `${input.value}px`;
+  };
+
+input.addEventListener("input", updateSpan);

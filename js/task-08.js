cf9562652b24c1cf361b6e7c7e019f29e937cb58
PatrycjaPiwowarczyk <piwowarczyk.p.a@gmail.com>
2022-12-11 14:19:29 +0100

    Task 8 completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const form = document.querySelector("form.login-form");
 
+const formChecking = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return window.alert("Please fill all the fields");
+  }
+  console.log(`{${email.name}: ${email.value}, ${password.name}: ${password.value}}`);
+  event.currentTarget.reset();
+};
+
+form.addEventListener("submit", formChecking);
